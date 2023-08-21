68af877e92e86e651fde79727b1d1ed36a1fe6cf
Dakshitha Hasindra <hasindradh@gmail.com>
2023-08-21 19:47:25 +0530

    Splash Screen Initialized

---
+++ personal-app/src/main/java/lk/ijse/dep11/AppInitializer.java
@@ -0,0 +1,42 @@
+package lk.ijse.dep11;
+
+import javafx.animation.RotateTransition;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/view/MainScene.fxml"));
+        Scene mainScene = new Scene(root);
+
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Splash Screen");
+
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        root.setBackground(Background.fill(Color.TRANSPARENT));
+        mainScene.setFill(Color.TRANSPARENT);
+
+        primaryStage.centerOnScreen();
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+
+    }
+}
---
+++ personal-app/src/main/java/lk/ijse/dep11/MainSceneController.java
@@ -0,0 +1,27 @@
+package lk.ijse.dep11;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.RotateTransition;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+public class MainSceneController {
+    public ImageView imgvSplashScreen;
+    public Label lblWelcome;
+
+
+    public void initialize(){
+        RotateTransition rotate = new RotateTransition(Duration.millis(2000), imgvSplashScreen);
+        rotate.setFromAngle(0);
+        rotate.setToAngle(30);
+        rotate.playFromStart();
+
+        FadeTransition fade = new FadeTransition(Duration.millis(1500),lblWelcome);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+        fade.playFromStart();
+    }
+
+
+}
